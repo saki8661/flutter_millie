5d22610d2a2c1707674ec7aabeb2b055bde8c7b3
saki8661 <saki8661@naver.com>
2023-10-17 02:20:26 +0900

    밀리의 서재 책 상세보기 페이지

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_millie/constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +9,376 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: BookDetail(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class BookDetail extends StatelessWidget {
+  const BookDetail({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.star_border, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert, color: Colors.black),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        children: [
+          Center(
+            child: Container(
+              height: 300,
+              width: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage("https://picsum.photos/200/300"),
+                  fit: BoxFit.cover,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey, // 그림자 색상
+                    offset: Offset(4, 4), // 그림자 위치
+                    blurRadius: 20, // 그림자 흐림 정도
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          SizedBox(height: 15),
+          Text(
+            "[독점] 향수가 된 식물들",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 10),
+          Text(
+            "장 클로드 엘레나 지금 / 카린 도어링 프로저 그림 / 이주영 옮김",
+            style: TextStyle(fontSize: 10, color: Colors.grey),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage("assets/avatar.png"),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "이 책이 담긴 서재",
+                        style: TextStyle(fontSize: 10, color: Colors.grey),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "2,380개",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Container(height: 60, width: 2, color: Colors.grey),
+                  Column(
+                    children: [
+                      Icon(Icons.comment_outlined, color: Colors.grey),
+                      SizedBox(height: 5),
+                      Text(
+                        "한 줄 리뷰",
+                        style: TextStyle(fontSize: 10, color: Colors.grey),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "4개",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Container(height: 60, width: 2, color: Colors.grey),
+                  Column(
+                    children: [
+                      Icon(Icons.edit_outlined, color: Colors.grey),
+                      SizedBox(height: 5),
+                      Text(
+                        "첫 포스트 작성하고",
+                        style: TextStyle(fontSize: 10, color: Colors.grey),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "+1밀리",
+                        style: TextStyle(fontSize: 10, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 10,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Icon(FontAwesomeIcons.award),
+                  SizedBox(width: 10),
+                  Text(
+                    "라이프스타일 분야 주간 베스트 3위",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 10,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "에르메스 조향사가 안내하는 향수 식물학 세계",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    "식물의 향과 향수에 관한 다채로운 이야기",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 2,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(FontAwesomeIcons.book),
+                      SizedBox(width: 10),
+                      Text(
+                        "밀리 완독지수",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(color: Colors.lightBlue),
+                    child: Center(child: Text("여기는 그래프자리야!!")),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(FontAwesomeIcons.circle, size: 15),
+                      Text(
+                        "마니아",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                      Container(height: 15, width: 2, color: Colors.grey),
+                      Text(
+                        "마니아들이 푹 빠진 읽을수록 보람 있는 책",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 10,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "부제",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 20),
+                      Text("에르메스 조향사가 안내하는 향수 식물학의 세계"),
+                      SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 2,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "책 소개",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 20),
+                      Text("에르메스, 시슬리, 까르띠에, 프레데릭 말, 입생로랑 ..."),
+                      Text("세계적인 향수를 탄생시킨 마스터 조향사"),
+                      Text("장 클로드 엘레나의 '향수 식물학' 세계로 초대합니다!"),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "더보기",
+                      style: TextStyle(
+                        color: kPointMillieColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ///낚였다 여기는 가로스크롤되는 자리였다!
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              color: Colors.amberAccent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(height: 5),
+                      Text(
+                        "카테고리",
+                        style: TextStyle(fontSize: 10, color: Colors.grey),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "라이프스타일",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Container(height: 60, width: 2, color: Colors.grey),
+                  Column(
+                    children: [
+                      SizedBox(height: 5),
+                      Text(
+                        "페이지",
+                        style: TextStyle(fontSize: 10, color: Colors.grey),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "228P",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Container(height: 60, width: 2, color: Colors.grey),
+                  Column(
+                    children: [
+                      SizedBox(height: 5),
+                      Text(
+                        "용량",
+                        style: TextStyle(fontSize: 10, color: Colors.grey),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "55.98MB",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+          /// 스크롤 끝자리는 여기!!!
+
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
